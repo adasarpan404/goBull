507cfc7bcd5b0e6393e20b956cb18ef9c40ca183
Arpan Das <dasarpan471@gmail.com>
2025-03-23 21:55:07 +0530

    queue.go completed

--- queue/queue.go
+++ queue/queue.go
@@ -1,1 +1,66 @@
 package queue
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Job struct {
+	ID        string    `json:"id"`
+	TimeStamp time.Time `json:"timestamp"`
+	Data      string    `json:"data"`
+	Delay     int       `json:"delay"`
+	Attempts  int       `json:"attempts"`
+}
+
+type Queue struct {
+	client *redis.Client
+	name   string
+	ctx    context.Context
+}
+
+func NewQueue(name string, redisAddr string) *Queue {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+	return &Queue{
+		client: rdb,
+		name:   name,
+		ctx:    context.Background(),
+	}
+}
+
+func (q *Queue) AddJob(job Job) error {
+	job.TimeStamp = time.Now()
+
+	jobJSON, err := json.Marshal(job)
+
+	if err != nil {
+		return err
+	}
+
+	if job.Delay > 0 {
+		return q.client.ZAdd(q.ctx, q.name+":delayed", redis.Z{
+			Score:  float64(time.Now().Unix() + int64(job.Delay)),
+			Member: jobJSON,
+		}).Err()
+	}
+
+	return q.client.LPush(q.ctx, q.name, jobJSON).Err()
+}
+
+func (q *Queue) GetJob() (*Job, error) {
+	jobstr, err := q.client.BRPop(q.ctx, 0*time.Second, q.name).Result()
+	if err != nil {
+		return nil, err
+	}
+	var job Job
+	err = json.Unmarshal([]byte(jobstr[1]), &job)
+	if err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
--- queue/worker.go
+++ queue/worker.go
@@ -1,8 +1,31 @@
 package queue
 
-import "time"
+import "fmt"
 
-type Job struct {
-	ID        string    `json:"id"`
-	TimeStamp time.Time `json:"timestamp"`
+type Worker struct {
+	queue    *Queue
+	handlers map[string]func(*Job) error
+}
+
+func NewWorker(queue *Queue) *Worker {
+	return &Worker{
+		queue:    queue,
+		handlers: make(map[string]func(*Job) error),
+	}
+}
+
+func (w *Worker) RegisterHandler(jobType string, handler func(*Job) error) {
+	w.handlers[jobType] = handler
+}
+
+func (w *Worker) start() {
+	fmt.Println("Worker Started......")
+
+	for {
+		job, err := w.queue.GetJob()
+
+		if err != nil {
+			fmt.Println()
+		}
+	}
 }
